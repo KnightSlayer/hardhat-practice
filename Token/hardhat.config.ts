4f984bd574c1f0fe3ae54470e929d371c75104f6
edgar <garinich.7@gmail.com>
2022-09-29 00:53:44 +0600

    deploy and verify

--- Token/hardhat.config.ts
+++ Token/hardhat.config.ts
@@ -2,9 +2,25 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const endpoint = process.env.URL;
+const privateKey = process.env.PRIVATE_KEY;
+const etherscanKey = process.env.ETHERSCAN_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: endpoint,
+      accounts: [`0x${privateKey}`],
+    },
+  },
+  etherscan: {
+    apiKey: etherscanKey,
+  },
 };
 
 export default config;
